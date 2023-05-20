2ce967a5de76a5af21143452869fc701e1552273
jakeersf <134116962+jakeersf@users.noreply.github.com>
2023-05-20 08:48:55 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents Diet Plan ")
+streamlit.header('Brekfast Menu')
+
+ streamlit.text('omega 3 & shake')
+ streamlit.text('fruits')
+streamlit.text('water')
