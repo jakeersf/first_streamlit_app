479692066e322a2cb8e5c7f62b1458f021cf5b0a
jakeersf <134116962+jakeersf@users.noreply.github.com>
2023-05-20 08:50:39 +0530

    Update streamlit_app.py
    error 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title("My Parents Diet Plan ")
 streamlit.header('Brekfast Menu')
 
- streamlit.text('omega 3 & shake')
+ streamlit.text('omega shake')
  streamlit.text('fruits')
 streamlit.text('water')
+ 
