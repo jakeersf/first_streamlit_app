7daefbd2177161088b06175f56c812d569602be2
jakeersf <134116962+jakeersf@users.noreply.github.com>
2023-05-20 11:02:45 +0530

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit
-
+import pandas
 streamlit.title("My Parents Diet Plan ")
 
 
-streamlit.header('Brekfast Menu')
-streamlit.text('omega shake')
-streamlit.text('fruits')
-streamlit.text('water')
- 
+streamlit.header('🍞Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
